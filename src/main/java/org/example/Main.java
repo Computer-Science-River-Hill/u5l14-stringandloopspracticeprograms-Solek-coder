cefa25a4f6b5f6180b81c8dbd5668e0ffec485c2
Solek <solekat@icloud.com>
2025-06-07 01:49:31 +0000

    AronTekie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example; //DO NOT DELETE
+import java.util.Scanner;
 
-//IMPORT STATEMENTS
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-//CLASS 
+        System.out.print("Enter a string: ");
+        String originalString = scanner.nextLine();
 
-//main METHOD
+        System.out.print("Enter beginning index: ");
+        int beginIndex = scanner.nextInt();
+
+        System.out.print("Enter ending index: ");
+        int endIndex = scanner.nextInt();
+
+        String substring = originalString.substring(beginIndex, endIndex);
+
+        System.out.println("Original string: " + originalString);
+        System.out.println("Substring: " + substring);
+
+        scanner.close();
+    }
+}
